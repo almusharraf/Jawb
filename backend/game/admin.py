6017920db8e21c707d281390632d6fa0271ecdcb
abdullah alotaishan <abdullah.alotaishan@kcl.ac.uk>
2025-02-22 20:25:01 +0000

    add image support

--- backend/game/admin.py
+++ backend/game/admin.py
@@ -1,3 +1,6 @@
+# game/admin.py
 from django.contrib import admin
+from .models import Category, Question
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Question)
